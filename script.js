87e9a9fa3521098a493d3f9ec678c38b64367692
M.Arif Manzoor <muhammadarifmanzoor15@gmail.com>
2026-08-18 10:46:17 +0500

    Game Complete

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const box_1 = document.getElementById("1")
-const box_2 = document.getElementById("2")
-const box_3 = document.getElementById("3")
-const box_4 = document.getElementById("4")
-const box_5 = document.getElementById("5")
-const box_6 = document.getElementById("6")
-const box_7 = document.getElementById("7")
-const box_8 = document.getElementById("8")
-const box_9 = document.getElementById("9")
+const box_1 = document.getElementById("box1")
+const box_2 = document.getElementById("box2")
+const box_3 = document.getElementById("box3")
+const box_4 = document.getElementById("box4")
+const box_5 = document.getElementById("box5")
+const box_6 = document.getElementById("box6")
+const box_7 = document.getElementById("box7")
+const box_8 = document.getElementById("box8")
+const box_9 = document.getElementById("box9")
 const game_status = document.getElementById("game-status")
 const clear = document.getElementById("clear-button")
 const all_boxes = [box_1 , box_2 , box_3 , box_4 , box_5 , box_6 , box_7 , box_8 , box_9]
+const x_score_display = document.getElementById("x-score")
+const o_score_display = document.getElementById("o-score")
+
+let game_running = true
+var x_score = 0
+var o_score = 0
+let x_turn = true;
 
 function isBoardFull() {
    for (let i = 0; i < all_boxes.length; i++) {
@@ -21,48 +28,49 @@ function isBoardFull() {
 }
 
 function clear_all() {
-   box_1.textContent = ""
-   box_2.textContent = ""
-   box_3.textContent = ""
-   box_4.textContent = ""
-   box_5.textContent = ""
-   box_6.textContent = ""
-   box_7.textContent = ""
-   box_8.textContent = ""
-   box_9.textContent = ""
+   all_boxes.forEach( function(box) {
+      box.textContent = ""
+      delete box.dataset.played
+   }
+   )
 }
-
-let game_running = true
 
 function victory(winner) {
    game_status.textContent = "Victory For " + winner
    game_running = false
+   if(winner == "X") {
+      x_score += 1
+      x_score_display.textContent = "X : " + x_score
+   } else if(winner == "O") {
+      o_score += 1
+      o_score_display.textContent = "O : " + o_score
+   }
 }
 
 function ending() {
    console.log("checking")
-   if(box_1.textContent !== "" && box_1.textContent == box_2.textContent && box_2.textContent == box_3.textContent) {
+   if(game_running && box_1.textContent !== "" && box_1.textContent == box_2.textContent && box_2.textContent == box_3.textContent) {
       console.log("Victory for " + box_1.textContent);
       victory(box_1.textContent)
-   }  else if (box_4.textContent !== "" && box_4.textContent == box_5.textContent && box_5.textContent == box_6.textContent) {
+   }  else if (game_running && box_4.textContent !== "" && box_4.textContent == box_5.textContent && box_5.textContent == box_6.textContent) {
       console.log("Victory for " + box_4.textContent);
       victory(box_4.textContent)
-   }  else if (box_7.textContent !== "" && box_7.textContent == box_8.textContent && box_8.textContent == box_9.textContent) {
+   }  else if (game_running && box_7.textContent !== "" && box_7.textContent == box_8.textContent && box_8.textContent == box_9.textContent) {
       console.log("Victory for " + box_7.textContent);
       victory(box_7.textContent)
-   }  else if (box_1.textContent !== "" && box_1.textContent == box_4.textContent && box_4.textContent == box_7.textContent) {
+   }  else if (game_running && box_1.textContent !== "" && box_1.textContent == box_4.textContent && box_4.textContent == box_7.textContent) {
       console.log("Victory for " + box_1.textContent);
       victory(box_1.textContent)
-   }  else if (box_2.textContent !== "" && box_2.textContent == box_5.textContent && box_5.textContent == box_8.textContent) {
+   }  else if (game_running && box_2.textContent !== "" && box_2.textContent == box_5.textContent && box_5.textContent == box_8.textContent) {
       console.log("Victory for " + box_2.textContent);
       victory(box_2.textContent)
-   }  else if (box_3.textContent !== "" && box_3.textContent == box_6.textContent && box_6.textContent == box_9.textContent) {
+   }  else if (game_running && box_3.textContent !== "" && box_3.textContent == box_6.textContent && box_6.textContent == box_9.textContent) {
       console.log("Victory for " + box_3.textContent);
       victory(box_3.textContent)
-   }  else if (box_1.textContent !== "" && box_1.textContent == box_5.textContent && box_5.textContent == box_9.textContent) {
+   }  else if (game_running && box_1.textContent !== "" && box_1.textContent == box_5.textContent && box_5.textContent == box_9.textContent) {
       console.log("Victory for " + box_1.textContent);
       victory(box_1.textContent)
-   }  else if (box_3.textContent !== "" && box_3.textContent == box_5.textContent && box_5.textContent == box_7.textContent) {
+   }  else if (game_running && box_3.textContent !== "" && box_3.textContent == box_5.textContent && box_5.textContent == box_7.textContent) {
       console.log("Victory for " + box_3.textContent);
       victory(box_3.textContent)
    } else if (isBoardFull()) {
@@ -77,51 +85,41 @@ clear.addEventListener("click" , function () {
 
 });
 
-let x_turn = false;
+all_boxes.forEach(function(box) {
 
-function turn(box)  {
-   if(x_turn == true && box.textContent === "" && game_running) {
-      box.textContent = "X";
-      x_turn = false;
-   } else if(box.textContent === "" && game_running){
-      box.textContent = "O";
-      x_turn = true;
-   }
-   ending()
-};
+   box.addEventListener("click" , function(event) {
+      if(!box.dataset.played && x_turn == true && game_running) {
+         box.dataset.played = true
+         box.style.color = "rgba(0, 0, 0, 0.82)"
+         box.textContent = "X";
+         x_turn = false;
+         ending()
 
-box_1.addEventListener("click" , function () {
-   turn(box_1)
-});
+      } else if(!box.dataset.played && game_running){
+         box.dataset.played = true
+         box.style.color = "rgba(0, 0, 0, 0.82)"
+         box.textContent = "O";
+         x_turn = true;
+         ending()
 
-box_2.addEventListener("click" , function () {
-   turn(box_2)
-});
+      }
+   })
 
-box_3.addEventListener("click" , function () {
-   turn(box_3)
-});
-
-box_4.addEventListener("click" , function () {
-   turn(box_4)   
-});
-
-box_5.addEventListener("click" , function () {
-   turn(box_5)   
-});
-
-box_6.addEventListener("click" , function () {
-   turn(box_6)   
-});
-
-box_7.addEventListener("click" , function () {
-   turn(box_7)   
-});
-
-box_8.addEventListener("click" , function () {
-   turn(box_8)   
-});
-
-box_9.addEventListener("click" , function () {
-   turn(box_9)   
-});
+   box.addEventListener("mouseover" , function() {
+      if(x_turn && !box.dataset.played && game_running) {
+         box.style.color = "rgba(1, 1, 102, 0.11)"
+         box.textContent = "X"
+      } else if(x_turn == false && !box.dataset.played && game_running) {
+         box.textContent = "O"
+         box.style.color = "rgba(1, 1, 102, 0.11)"
+      }
+   })
+   
+   box.addEventListener("mouseout" , function() {
+      if(!box.dataset.played) {
+         box.textContent = ""
+      } else {
+         box.style.color = "rgba(0, 0, 0, 0.82)"
+      }
+   })
+}) 
